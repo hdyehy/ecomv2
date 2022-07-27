28b02f41577cf1d83a6c8344ae85cf5b8adcb841
hdyehy <trigo.matthew@outlook.com>
2022-07-27 11:28:42 -0500

    final deployment

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "hhttps://matthewtrigoecomapi.herokuapp.com/api";
+const BASE_URL = "https://matthewtrigoecomapi.herokuapp.com/api";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTAwZTVhYWFmOWEzMmU3N2M4YzE4MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1ODg5OTk0NCwiZXhwIjoxNjU5MTU5MTQ0fQ.Tb9VpME8y4oqSCzvknJi_URrYrCjVhzxVMdARbI6RTI";
 
